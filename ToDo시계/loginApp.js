d2c2a1657314b134db926dad1931263aa35bd0b6
sarumani <qkre2018@gmail.com>
2023-07-07 16:02:09 +0900

    과제 완료

--- ToDo시계/loginApp.js
+++ ToDo시계/loginApp.js
@@ -9,40 +9,42 @@ const userDB = [
 
 // 메인 화면 태그 요소들
 const tags = {
-  bodyTag: document.querySelector("body"),
-  userIdTag: document.querySelector(".container .loginSection #id"),
-  userPwTag: document.querySelector(".container .loginSection #pw"),
-  loginBtnTag: document.querySelector(".container .actionSection .loginBtn"),
-  findPwBtnTag: document.querySelector(".container .actionSection .findPw"),
+  bodyTag: document.querySelector("body"), // body 요소
+  userIdTag: document.querySelector(".container .loginSection #id"), // 아이디 입력 필드 요소
+  userPwTag: document.querySelector(".container .loginSection #pw"), // 패스워드 입력 필드 요소
+  loginBtnTag: document.querySelector(".container .actionSection .loginBtn"), // 로그인 버튼 요소
+  findPwBtnTag: document.querySelector(".container .actionSection .findPw"), // 비밀번호 찾기 버튼 요소
 };
 
 // 회원가입 팝업 태그 요소들
 const registerPopUpTags = {
-  popUpTag: document.querySelector(".container .header #register"),
-  userFirstNameTag: document.querySelector(".registerProfile #userFirstName"),
-  userLastNameTag: document.querySelector(".registerProfile #userLastName"),
-  userIdTag: document.querySelector(".registerProfile #userID"),
-  userPwTag: document.querySelector(".registerProfile #userPW"),
-  registerBtnTag: document.querySelector(".registerProfile .registerBtn"),
+  popUpTag: document.querySelector(".container .header #register"), // 회원가입 팝업 체크박스 요소
+  userFirstNameTag: document.querySelector(".registerProfile #userFirstName"), // 이름 입력 필드 요소
+  userLastNameTag: document.querySelector(".registerProfile #userLastName"), // 성 입력 필드 요소
+  userIdTag: document.querySelector(".registerProfile #userID"), // 아이디 입력 필드 요소
+  userPwTag: document.querySelector(".registerProfile #userPW"), // 패스워드 입력 필드 요소
+  registerBtnTag: document.querySelector(".registerProfile .registerBtn"), // 회원가입 버튼 요소
+  isValidIdTag: document.querySelector(".isValidID"), // 아이디 유효성 검사 결과 요소
+  isValidPasswordTag: document.querySelector(".isValidPassword"), // 패스워드 유효성 검사 결과 요소
 };
 
 // 비밀번호 찾기 팝업 태그 요소들
 const findPopUpTags = {
-  popUpTag: document.querySelector(".container .actionSection #findPW"),
-  userFirstNameTag: document.querySelector(".findPassword #userFirstName"),
-  userLastNameTag: document.querySelector(".findPassword #userLastName"),
-  userIdTag: document.querySelector(".findPassword #userId"),
-  findBtnTag: document.querySelector(".findPassword .findBtn"),
+  popUpTag: document.querySelector(".container .actionSection #findPW"), // 비밀번호 찾기 팝업 체크박스 요소
+  userFirstNameTag: document.querySelector(".findPassword #userFirstName"), // 이름 입력 필드 요소
+  userLastNameTag: document.querySelector(".findPassword #userLastName"), // 성 입력 필드 요소
+  userIdTag: document.querySelector(".findPassword #userId"), // 아이디 입력 필드 요소
+  findBtnTag: document.querySelector(".findPassword .findBtn"), // 비밀번호 찾기 버튼 요소
 };
 
 // 로그인 입력 유효성 검사
 function checkValidity() {
   if (!tags.userIdTag.checkValidity()) {
-    alert("아이디를 입력하세요!");
+    alert("아이디를 입력하세요!"); // 아이디가 유효하지 않은 경우 경고창 출력
     return false;
   }
   if (!tags.userPwTag.checkValidity()) {
-    alert("패스워드를 입력하세요!");
+    alert("패스워드를 입력하세요!"); // 패스워드가 유효하지 않은 경우 경고창 출력
     return false;
   }
   return true;
@@ -51,24 +53,24 @@ function checkValidity() {
 // 회원가입 유효성 검사
 function registerValidity() {
   if (!registerPopUpTags.userFirstNameTag.checkValidity()) {
-    alert("이름을 입력하세요!");
+    alert("이름을 입력하세요!"); // 이름이 유효하지 않은 경우 경고창 출력
     return false;
   }
   if (!registerPopUpTags.userLastNameTag.checkValidity()) {
-    alert("성을 입력하세요!");
+    alert("성을 입력하세요!"); // 성이 유효하지 않은 경우 경고창 출력
     return false;
   }
   if (!registerPopUpTags.userIdTag.checkValidity()) {
-    alert("아이디를 입력하세요!");
+    alert("아이디를 입력하세요!"); // 아이디가 유효하지 않은 경우 경고창 출력
     return false;
   }
   if (!registerPopUpTags.userPwTag.checkValidity()) {
-    alert("패스워드를 입력하세요!");
+    alert("패스워드를 입력하세요!"); // 패스워드가 유효하지 않은 경우 경고창 출력
     return false;
   } else {
     const pattern = /[^a-zA-Z0-9]/;
     if (!pattern.test(registerPopUpTags.userPwTag.value)) {
-      alert("패스워드에 반드시 특수문자가 포함되어야 합니다!");
+      alert("패스워드에 반드시 특수문자가 포함되어야 합니다!"); // 패스워드에 특수문자가 없는 경우 경고창 출력
       return false;
     }
   }
@@ -78,15 +80,15 @@ function registerValidity() {
 // 비밀번호 찾기 유효성 검사
 function findPwValidity() {
   if (!findPopUpTags.userFirstNameTag.checkValidity()) {
-    alert("이름을 입력하세요!");
+    alert("이름을 입력하세요!"); // 이름이 유효하지 않은 경우 경고창 출력
     return false;
   }
   if (!findPopUpTags.userLastNameTag.checkValidity()) {
-    alert("성을 입력하세요!");
+    alert("성을 입력하세요!"); // 성이 유효하지 않은 경우 경고창 출력
     return false;
   }
   if (!findPopUpTags.userIdTag.checkValidity()) {
-    alert("아이디를 입력하세요!");
+    alert("아이디를 입력하세요!"); // 아이디가 유효하지 않은 경우 경고창 출력
     return false;
   }
   return true;
@@ -148,10 +150,10 @@ function registerUser() {
     }
   }
 }
-
-// 비밀번호 찾기
+// 비밀번호 찾기 함수
 function findPassword() {
   if (findPwValidity()) {
+    // 사용자 정보가 일치하는 경우
     if (
       userDB.some((user) => user.id === findPopUpTags.userIdTag.value) &&
       userDB.some(
@@ -161,23 +163,61 @@ function findPassword() {
         (user) => user.lastName === findPopUpTags.userLastNameTag.value
       )
     ) {
+      // 해당 사용자의 비밀번호를 알림으로 보여줌
       const targetIndex = userDB.findIndex(
         (user) => user.id === findPopUpTags.userIdTag.value
       );
       alert(`비밀번호는 ${userDB[targetIndex].pw}입니다!`);
-      clearTags();
-      findPopUpTags.popUpTag.checked = false;
+      clearTags(); // 입력 필드 초기화
+      findPopUpTags.popUpTag.checked = false; // 비밀번호 찾기 팝업 닫기
     } else {
       alert("회원 정보를 찾을 수 없습니다!");
     }
   }
 }
 
+// 로그인 버튼 클릭 이벤트 처리
 tags.loginBtnTag.addEventListener("click", checkUser);
+
+// 회원가입 시 아이디 유효성 검사
+registerPopUpTags.userIdTag.onkeyup = function () {
+  if (registerPopUpTags.userIdTag.value.length < 1) {
+    registerPopUpTags.isValidIdTag.classList.add("hide");
+    return;
+  }
+
+  if (userDB.some((user) => user.id !== registerPopUpTags.userIdTag.value)) {
+    // 아이디가 사용 가능한 경우
+    registerPopUpTags.isValidIdTag.classList.remove("hide");
+    registerPopUpTags.isValidIdTag.innerText = "사용 가능한 아이디 입니다.";
+  } else {
+    // 아이디가 이미 존재하는 경우
+    registerPopUpTags.isValidIdTag.innerText = "이미 존재하는 아이디 입니다.";
+  }
+};
+
+// 회원가입 시 비밀번호 유효성 검사
+registerPopUpTags.userPwTag.onkeyup = function () {
+  const pattern = /[^a-zA-Z0-9]/;
+  if (!pattern.test(registerPopUpTags.userPwTag.value)) {
+    // 비밀번호가 올바른 형태가 아닌 경우
+    registerPopUpTags.isValidPasswordTag.classList.remove("hide");
+    registerPopUpTags.isValidPasswordTag.innerText =
+      "올바른 비밀번호 형태가 아닙니다.";
+  } else {
+    // 비밀번호가 올바른 형태인 경우
+    registerPopUpTags.isValidPasswordTag.innerText =
+      "사용 가능한 비밀번호 입니다.";
+  }
+};
 
 // 엔터 키로 로그인하기
 tags.bodyTag.addEventListener("keydown", (e) => {
   if (e.keyCode == 13) checkUser();
 });
+
+// 회원가입 버튼 클릭 이벤트 처리
 registerPopUpTags.registerBtnTag.addEventListener("click", registerUser);
+
+// 비밀번호 찾기 버튼 클릭 이벤트 처리
 findPopUpTags.findBtnTag.addEventListener("click", findPassword);
--- ToDo시계/mainApp.js
+++ ToDo시계/mainApp.js
@@ -1,30 +1,69 @@
+// URL에서 userId 추출
 const urlParams = new URLSearchParams(window.location.search);
 const userId = urlParams.get("userId");
 
+// DOM 요소들을 선택하는 객체(tags) 정의
 const tags = {
   // DOM 요소들
-  titleTag: document.querySelector(".whole .title"),
-  wholeTag: document.querySelector(".whole"),
-  alarmTimeTag: document.querySelector(".container .inputSection .time"),
-  toDoItemTag: document.querySelector(".container .inputSection .item"),
-  addBtnTag: document.querySelector(".container .inputSection .add"),
-  toDoTableTag: document.querySelector(".items .todoList"),
-  logoutBtnTag: document.querySelector(".whole .logout"),
+  bodyTag: document.querySelector("body"), // body 요소
+  titleTag: document.querySelector(".whole .title"), // .whole .title 요소
+  wholeTag: document.querySelector(".whole"), // .whole 요소
+  alarmTimeTag: document.querySelector(".container .inputSection .time"), // .container .inputSection .time 요소
+  toDoItemTag: document.querySelector(".container .inputSection .item"), // .container .inputSection .item 요소
+  addBtnTag: document.querySelector(".container .inputSection .add"), // .container .inputSection .add 요소
+  toDoTableTag: document.querySelector(".items .todoList"), // .items .todoList 요소
+  logoutBtnTag: document.querySelector(".whole .logout"), // .whole .logout 요소
 
-  // 배터리가 0이되면 동작하는 함수
+  // 할 일 목록을 비활성화하는 함수
   disableToDoList() {
-    this.wholeTag.style.backgroundColor = "black";
-    this.alarmTimeTag.readOnly = true;
-    this.toDoItemTag.readOnly = true;
-    this.addBtnTag.disabled = true;
+    this.wholeTag.style.backgroundColor = "black"; // 배경색을 검정으로 변경
+    this.alarmTimeTag.readOnly = true; // 알람 시간 입력 필드 비활성화
+    this.toDoItemTag.readOnly = true; // 할 일 입력 필드 비활성화
+    this.addBtnTag.disabled = true; // 추가 버튼 비활성화
+  },
+
+  // 시간을 확인하여 할 일 목록을 체크하는 함수
+  checkTime() {
+    const toDoItems = document.querySelectorAll(".items .todoList .todo"); // 할 일 목록에서 모든 할 일 요소 선택
+    for (let i = 0; i < toDoItems.length; i++) {
+      if (
+        toDoItems[i].firstChild.textContent ===
+        header.timeTag.textContent.slice(0, 5)
+      ) {
+        toDoItems[i].remove(); // 해당 시간과 일치하는 할 일 제거
+        this.alertBackground(0); // 배경색 애니메이션 실행
+      }
+    }
+  },
+
+  // 배경색 애니메이션 실행하는 함수
+  alertBackground(count) {
+    if (count >= 5) {
+      tags.bodyTag.style.background = "#525fe1"; // 배경색을 파란색으로 변경
+      return;
+    }
+
+    if (count % 2 == 0) {
+      tags.bodyTag.style.background = "red"; // 배경색을 빨간색으로 변경
+    } else {
+      tags.bodyTag.style.background = "blue"; // 배경색을 파란색으로 변경
+    }
+
+    setTimeout(() => {
+      this.alertBackground(count + 1);
+    }, 500);
   },
 };
 
 // 새로운 할 일을 추가하는 함수
 function addToDoItem() {
-  const toDos = document.querySelectorAll(".items .todoList .todo");
-  if (toDos.length < 3) {
-    // 새로운 요소 생성
+  const toDos = document.querySelectorAll(".items .todoList .todo"); // 할 일 목록에서 모든 할 일 요소 선택
+  if (
+    toDos.length < 3 &&
+    tags.alarmTimeTag.value !== "" &&
+    tags.toDoItemTag.value !== ""
+  ) {
+    // 새로운 할 일 요소 생성
     const newDivTag = document.createElement("div");
     newDivTag.classList.add("todo");
 
@@ -37,7 +76,7 @@ function addToDoItem() {
     const newBtnTag = document.createElement("button");
     newBtnTag.innerText = "❌";
     newBtnTag.addEventListener("click", (event) => {
-      event.target.parentElement.remove();
+      event.target.parentElement.remove(); // 할 일 삭제 버튼 클릭 시 할 일 요소 제거
     });
 
     // CSS 스타일 적용
@@ -56,21 +95,21 @@ function addToDoItem() {
 
     newDivTag.style.marginBottom = "5px";
 
-    tags.toDoTableTag.appendChild(newDivTag);
+    tags.toDoTableTag.appendChild(newDivTag); // 할 일 목록에 새로운 할 일 요소 추가
   }
 }
 
 // 로그아웃 함수 -> 로그인 페이지로 이동
 function logout() {
-  window.location.href = "loginPage.html";
+  window.location.href = "loginPage.html"; // 로그인 페이지로 이동
 }
 
 // 사용자 아이디를 제목으로 설정
 tags.titleTag.innerText = `Hello! ${userId}`;
 
-tags.addBtnTag.addEventListener("click", addToDoItem);
+tags.addBtnTag.addEventListener("click", addToDoItem); // 할 일 추가 버튼 클릭 시 addToDoItem 함수 실행
 
-tags.logoutBtnTag.addEventListener("click", logout);
+tags.logoutBtnTag.addEventListener("click", logout); // 로그아웃 버튼 클릭 시 logout 함수 실행
 
 // 24시간 형태의 input을 받기 위해 flatpickr 사용
 tags.alarmTimeTag.flatpickr({
@@ -103,8 +142,17 @@ const header = {
   },
 };
 
+header.timeTag.textContent = new Date().toLocaleTimeString("en-Us", {
+  hour12: false,
+});
+
 // 1초당 한 번씩 시간과 배터리가 업데이트 됨
 const interval = setInterval(() => {
   header.updateTime();
   header.updateBatteryLevel();
+  tags.checkTime();
 }, 1000);
+
+tags.bodyTag.addEventListener("keydown", (e) => {
+  if (e.keyCode == 13) addToDoItem();
+});
